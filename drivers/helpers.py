3817fa96e384c4b31efae70e2e12ea6f67d05f3a
Silke <smg374@cornell.edu>
2020-07-30 18:33:31 -0400

    extended helpers.py for dommaschk

--- drivers/helpers.py
+++ drivers/helpers.py
@@ -2,3 +2,8 @@ import pyparticle as pp
 def get_antoine_field(Btin, epsilon=0.32, kappa=1.7, delta=0.33, A=-0.2):
   B = pp.AntoineField(epsilon, kappa, delta, A, Btin);
   return B
+  
+def get_dommaschk_field(alpha= 1.98):
+  B = pp.DommaschkField(alpha);
+  return B
+  
